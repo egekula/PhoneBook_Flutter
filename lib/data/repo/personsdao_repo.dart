74becb1888b81c5583188ae4a76f0f23a1dbcb83
Ege Kula <egekula3@gmail.com>
2024-04-04 23:50:06 +0300

    +SQlite is used. +SQlite file added.

--- lib/data/repo/personsdao_repo.dart
+++ lib/data/repo/personsdao_repo.dart
@@ -1,29 +1,39 @@
 import 'package:phonebook_app/data/entity/persons.dart';
+import 'package:phonebook_app/sqlite/veritabani_yardimcisi.dart';
 
 class PersonsDaoRepository{
   Future<void> Save(String person_name,String person_number) async {
-    print("$person_name - $person_number");
+    var db = await VeritabaniYardimcisi.veritabaniErisim();
+    var newPerson = Map<String,dynamic>();
+    newPerson["person_name"] = person_name;
+    newPerson["person_number"] = person_number;
+    await db.insert("Persons", newPerson);
   }
   Future<void> Update(int person_id,String person_name,String person_number) async {
-    print("$person_id-$person_name - $person_number");
+    var db = await VeritabaniYardimcisi.veritabaniErisim();
+    var updatedPerson = Map<String,dynamic>();
+    updatedPerson["person_name"] = person_name;
+    updatedPerson["person_number"] = person_number;
+    await db.update("Persons", updatedPerson,where: "person_id = ?",whereArgs: [person_id]);
   }
   Future <List<Persons>> LoadPersons() async {
-    var personsList = <Persons>[];
-    var p1 = Persons(person_id: 1, person_name: "James", person_number: "1111");
-    var p2 = Persons(person_id: 2, person_name: "Anna", person_number: "2222");
-    var p3 = Persons(person_id: 3, person_name: "Jamal", person_number: "3333");
-    personsList.add(p1);
-    personsList.add(p2);
-    personsList.add(p3);
-    return personsList;
+    var db = await VeritabaniYardimcisi.veritabaniErisim();
+    List<Map<String,dynamic>> maps = await db.rawQuery("SELECT * FROM Persons");
+    return List.generate(maps.length, (index){
+      var satir = maps[index];
+      return Persons(person_id: satir["person_id"], person_name: satir["person_name"], person_number: satir["person_number"]);
+    });
   }
   Future <List<Persons>> Search(String wordSearched) async {
-    var personsList = <Persons>[];
-    var p1 = Persons(person_id: 1, person_name: "James", person_number: "1111");
-    personsList.add(p1);
-    return personsList;
+    var db = await VeritabaniYardimcisi.veritabaniErisim();
+    List<Map<String,dynamic>> maps = await db.rawQuery("SELECT * FROM Persons WHERE person_name like '%$wordSearched%'");
+    return List.generate(maps.length, (index){
+      var satir = maps[index];
+      return Persons(person_id: satir["person_id"], person_name: satir["person_name"], person_number: satir["person_number"]);
+    });
   }
   Future <void> Delete(int personID) async {
-    print("$personID");
+    var db = await VeritabaniYardimcisi.veritabaniErisim();
+    await db.delete("Persons",where: "person_id = ?",whereArgs: [personID]);
   }
 }
